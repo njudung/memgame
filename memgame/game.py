f69ed6b103e549a6956c31424c31a5efd2d089d5
= <=>
2022-05-11 13:00:22 +0200

    Basic graphics.

--- memgame/game.py
+++ memgame/game.py
@@ -1,4 +1,4 @@
-from guizero import App
+from guizero import App, Drawing
 from gpiozero import Button, LED
 from signal import pause
 from . import logger
@@ -13,8 +13,14 @@ class Game():
             'buttons': [Button(10), Button(9), Button(11)],
             'leds': [LED(17), LED(27), LED(22)],
             'combination': [],
-            'position': 0
+            'position': 0,
+            'highscore': Game.load_highscore()
             }
+
+
+        self.app = App(title="Memgame", bg=(30, 30, 30))
+        self.app.set_full_screen()
+        self.drawing = Drawing(self.app, width="fill", height="fill")
 
         self.ctx = Context(self, WaitState())
 
@@ -24,7 +30,7 @@ class Game():
             button.when_pressed = lambda btn=button, num=i: self.ctx.on_event({'name': 'button_pressed', 'button': btn, 'num': num})
             button.when_released = lambda btn=button, num=i: self.ctx.on_event({'name': 'button_released', 'button': btn, 'num': num})
 
-        pause()
+        self.app.display()
     
 
     def turn_off_leds(self):
@@ -45,10 +51,39 @@ class Game():
             sleep(dt)
             self.turn_off_leds()
             sleep(dt)
-       
+    
+    
+    @staticmethod
+    def load_highscore():
+        rv = []
+
+        try:
+            with open("highscore.txt") as fp:
+                for line in fp.readlines():
+                    rv.append(line.strip().split("|"))
+        except FileNotFoundError:
+            pass
+
+        return rv
+
+
+    @staticmethod
+    def save_highscore(scores):
+        with open("highscores.txt", "w") as fp:
+            for name, score in scores.sort(lambda item: item[1]):
+                fp.write(f"{name}|{score}")
+            
+
 
 class WaitState(State):
     def on_event(self, event):
+        if event['name'] == 'init':
+            dwg = self.drawing
+            dwg.clear()
+            dwg.text(10, 10, "MEMGAME", color=(250, 250, 250), size=50)
+            dwg.text(10, 100, "Tryck på en knapp", color=(250, 250, 250), size=30)
+
+
         if event['name'] == 'button_pressed':
             logger.info('Starting game.')
             self.game_state['combination'] = []  # Reset combination
@@ -57,34 +92,68 @@ class WaitState(State):
         return self
 
 
+class StartGameState(State):
+    def on_event(self, event):
+        if event['name'] == 'init':
+            dwg = self.drawing
+            dwg.clear()
+            dwg.text(10, 10, "", color=(250, 250, 250), size=50)
+            dwg.text(10, 100, "Starting game", color=(250, 250, 250), size=30)
+
+            self.game_state['combination'] = []
+            self.game_state['position'] = 0
+    
+            sleep(1)
+    
+            return ShowCombinationState()
+    
+        return self
+    
+
 class ShowCombinationState(State):
     def on_event(self, event):
-        self.game_state['combination'].append(randint(0, len(self.game_state['leds']) - 1))
-        self.game_state['position'] = 0
+        if event['name'] == 'init':
+            dwg = self.drawing
+            dwg.clear()
+            dwg.text(10, 10, "", color=(250, 250, 250), size=50)
+            dwg.text(10, 100, "Memorera sekvensen!", color=(250, 250, 250), size=30)
 
-        logger.debug('Combination updated to: {}'.format(', '.join([str(v) for v in self.game_state['combination']])))
+            nv = randint(0, len(self.game_state['leds']) - 1)
+            if len(self.game_state['combination']) and nv == self.game_state['combination'][-1]:
+                nv = randint(0, len(self.game_state['leds']) - 1)
 
-        # Flash all leds once to indicate start of sequence.
-        sleep(1)
-        self.game.turn_on_leds()
-        sleep(0.1)
-        self.game.turn_off_leds()
+            self.game_state['combination'].append(nv)
+            logger.debug('Combination updated to: {}'.format(', '.join([str(v) for v in self.game_state['combination']])))
 
-        
-        for v in self.game_state['combination']:
-            self.game.turn_off_leds()
-            sleep(0.1)
-
-            self.game_state['leds'][v].on()
+            # Flash all leds once to indicate start of sequence.
             sleep(1)
-
+            self.game.turn_on_leds()
+            sleep(0.1)
             self.game.turn_off_leds()
+
+            
+            for v in self.game_state['combination']:
+                self.game.turn_off_leds()
+                sleep(0.1)
+
+                self.game_state['leds'][v].on()
+                sleep(1)
+
+                self.game.turn_off_leds()
 
         return RepeatCombinationState()
 
 
 class RepeatCombinationState(State):
     def on_event(self, event):
+        if event['name'] == 'init':
+            dwg = self.drawing
+            dwg.clear()
+            dwg.text(10, 100, "Upprepa sekvensen", color=(250, 250, 250), size=30)
+
+            self.game_state['position'] = 0
+
+
         if event['name'] == 'button_pressed':
             """ Check for correct button press. """
             self.game_state['leds'][event['num']].on()
@@ -92,15 +161,16 @@ class RepeatCombinationState(State):
             # Correct button?
             if event['num'] == self.game_state['combination'][self.game_state['position']]:
                 self.game_state['position'] += 1
+
+                if self.game_state['position'] >= len(self.game_state['combination']):
+                    return ShowCombinationState()
+
             else:
                 logger.info('Wrong button. Game over.')
                 
                 self.game.flash_leds()
                 
-                return GameOverState()                
-                
-            if self.game_state['position'] >= len(self.game_state['combination']):
-                return ShowCombinationState()
+                return GameOverState()                                
         
         if event['name'] == 'button_released':
             """ Flash once to confirm correct button press. """
@@ -116,6 +186,11 @@ class RepeatCombinationState(State):
 
 class GameOverState(State):
     def on_event(self, event):
+        if event['name'] == 'init':
+            dwg = self.drawing
+            dwg.clear()
+            dwg.text(10, 10, "GAME OVER!", color=(250, 250, 250), size=50)
+        
         if event['name'] == 'button_pressed':
             return WaitState()
         
--- memgame/state_machine.py
+++ memgame/state_machine.py
@@ -6,27 +6,43 @@ class Context():
         self._state = state
         self._state.context = self
         self.game = game
+        self._state.on_event({'name': 'init'})
 
 
     def on_event(self, event):
         logger.debug('Event: {}'.format(event))
-        self._state = self._state.on_event(event)
-        self._state.context = self
+        new_state = self._state.on_event(event)
         
+        if not self._state is new_state:
+            self._state = new_state
+            self._state.context = self
+            self._state.on_event({'name': 'init'})
+
 
 class State():
     def __init__(self):
         logger.debug(f'Processing current state: {str(self)}')
         self.context = None
 
-    
+
     def on_event(self, event):
         raise NotImplementedError()
 
 
     @property
+    def drawing(self):
+        raise Exception()
+
+
+    @drawing.getter
+    def drawing(self):
+        return self.context.game.drawing
+
+
+    @property
     def game_state(self):
         raise Exception()
+
 
     @game_state.getter
     def game_state(self):
@@ -36,6 +52,7 @@ class State():
     @property
     def game(self):
         raise Exception()
+
     
     @game.getter
     def game(self):
